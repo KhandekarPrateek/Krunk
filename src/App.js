27cfe4a8147999671b43d55bb01a9fc274230d2c
KhandekarPrateek <prateekkhandekar01@gmail.com>
2024-04-12 11:39:35 +0530

    bag comp

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Container, Row } from "reactstrap";
 import Header from "./pages/Header";
 import Chat from "./pages/Chat";
 import Tags from "./pages/Tags";
+import Bag from "./pages/Bag";
 function App() {
   return (
     <Container className="main-container border rounded-2 mt-3  bg-white">
       <Header />
       <Chat />
       <Tags />
+      <Bag />
     </Container>
   );
 }
---
+++ src/pages/Bag/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardSubtitle,
+  CardText,
+  CardTitle,
+  Row,
+  Col,
+} from "reactstrap";
+import BagImage from "../../assets/Bag.png";
+import Vector from "../../assets/Vector.png";
+import Chevron from "../../assets/chevron-right.png";
+
+const Bag = () => {
+  return (
+    <Card className="card-bag p-2 ">
+      <CardBody>
+        <Row className="bag-image-container p-4 ">
+          <Col
+            className="d-flex align-items-center justify-content-center "
+            sm={3}
+          >
+            <img alt="Sample" src={BagImage} />
+          </Col>
+          <Col sm={9}>
+            <Row>
+              <Col sm={9}>
+                <CardTitle tag="h5">Bags on Timpu</CardTitle>
+              </Col>
+              <Col
+                sm={3}
+                className="d-flex justify-content-start align-items-start"
+              >
+                <img src={Vector} className="vector-image" />
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={9}>
+                <CardSubtitle className="text-muted" tag="h6">
+                  1123 products
+                </CardSubtitle>
+              </Col>
+              <Col
+                sm={3}
+                className="d-flex justify-content-start align-items-start"
+              >
+                <img src={Chevron} className="vector-image" />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <CardText>
+            Or set filter and help us choose the best bag for you.
+          </CardText>
+        </Row>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default Bag;
